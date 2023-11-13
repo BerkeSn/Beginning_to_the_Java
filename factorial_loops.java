fddc5154e58fc60af509c6ff164f9f43195b8ee7
Berke Şen <113352134+BerkeSn@users.noreply.github.com>
2023-11-13 17:52:12 +0300

    Update factorial_loops.java

--- factorial_loops.java
+++ factorial_loops.java
@@ -19,16 +19,13 @@ public class d9 {
 
 	// Rest of the codes are doing the same thing with for loop
 
-		System.out.print("Input a number: ");
-		int a= scan.nextInt();
-		int k = a;
-		int factorial = 1;
-		
+		System.out.print("Input a integer: ");
+		int a = scan.nextInt();
+		int faktoriel = 1;
 		for(;a>=1;a--) {
-			factorial *=a;
-			
+			faktoriel *=a;
 		}
-		System.out.println(k + " sayısının faktörieli: " + factorial);
+		System.out.println(faktoriel);
 	
 	}	
 }
