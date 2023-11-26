6c5c0d9ab61189b155ace08eaeefe4855dd32326
Berke Şen <113352134+BerkeSn@users.noreply.github.com>
2023-11-26 16:43:26 +0300

    Create sum_of_numbers.java

---
+++ sum_of_numbers.java
@@ -0,0 +1,8 @@
+		int total=0;
+		
+		for(int a=1; a<=10;a++){
+			total += a;
+			System.out.println("Total of from 1 to " + a + " equal to: " + total) ;
+		}
+		
+		System.out.println("Total: " + total);
